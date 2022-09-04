5fb58eb2ef4c0c806d9d49eb8f9b4e94231099bd
Daniel <daniel.laskarzhevsky@gmail.com>
2022-09-04 17:32:07 -0400

    Host resolution fix

--- m8/networking/backend.tf
+++ m8/networking/backend.tf
@@ -3,7 +3,8 @@
 ##################################################################################
 terraform {
   backend "consul" {
-    address = "host.docker.internal:8500"
+    #address = "host.docker.internal:8500"
+    address = "http://127.0.0.1/:8500"
     scheme  = "http"
   }
 }
